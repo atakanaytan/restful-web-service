e556a0d4c4412c9f02fb46628ee5182055181ed1
atakanaytan <aytan.atakan2@gmail.com>
2020-05-13 23:22:02 +0300

    method for generating random publicUserId was added in the shared folder

--- src/main/java/com/mobile/app/ws/mobileappws/service/impl/UserServiceImpl.java
+++ src/main/java/com/mobile/app/ws/mobileappws/service/impl/UserServiceImpl.java
@@ -1,9 +1,9 @@
 package com.mobile.app.ws.mobileappws.service.impl;
 
-import com.fasterxml.jackson.databind.util.BeanUtil;
 import com.mobile.app.ws.mobileappws.io.entity.UserEntity;
 import com.mobile.app.ws.mobileappws.io.repository.UserRepository;
 import com.mobile.app.ws.mobileappws.service.UserService;
+import com.mobile.app.ws.mobileappws.shared.Utils;
 import com.mobile.app.ws.mobileappws.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,13 +15,21 @@ public class UserServiceImpl implements UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    Utils utils;
+
     @Override
     public UserDto createUser(UserDto user) {
+
+        if (userRepository.findUserByEmail(user.getEmail()) != null) {
+            throw new RuntimeException("Record already exist");
+        }
 
         UserEntity userEntity = new UserEntity();
         BeanUtils.copyProperties(user, userEntity);
 
-        userEntity.setUserId("testUserId");
+        String publicUserId = utils.generateUserId(30);
+        userEntity.setUserId(publicUserId);
         userEntity.setEncryptedPassword("Test");
 
         UserEntity storedUserDetails = userRepository.save(userEntity);
