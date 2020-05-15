2598e6ad614fce7d25064b1834ca45a1a4895f87
atakanaytan <aytan.atakan2@gmail.com>
2020-05-16 01:32:41 +0300

    Jwt authorization filter was added. Checks header if it's has a valid token

--- src/main/java/com/mobile/app/ws/mobileappws/ui/controller/UserController.java
+++ src/main/java/com/mobile/app/ws/mobileappws/ui/controller/UserController.java
@@ -6,10 +6,7 @@ import com.mobile.app.ws.mobileappws.ui.model.request.UserDetailsRequestModel;
 import com.mobile.app.ws.mobileappws.ui.model.response.UserRest;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -18,6 +15,12 @@ public class UserController {
 
     @Autowired
     UserService userService;
+
+    @GetMapping
+    public String getUser(){
+
+        return "get user was called";
+    }
 
     @PostMapping
     public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) {
